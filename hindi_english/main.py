b5588a6f576759cc6261d53d5ff7a7f5ff660b51
jayant-ism <52823326+jayant-ism@users.noreply.github.com>
2020-09-12 16:48:45 +0530

    working on backend

--- hindi_english/main.py
+++ hindi_english/main.py
@@ -1,7 +1,63 @@
+
+#inport some stuff
 import subprocess
+import json 
 
 
+#the number of extra keywords declared 
+count = 0 
 
+
+# dictionary for hindi to english words
+dic = {} 
+
+
+# dictionary for english to hindi words 
+rev_dic = {} 
+
+
+#loading the global dictionary later include the values for the included libraries 
+with open('map.json') as json_file: 
+     
+    
+    dic = json.load(json_file) 
+
+
+#loding the reverse dictionary 
+with open('rev_map.json') as json_file: 
+    
+    
+    rev_dic = json.load(json_file) 
+
+
+# for converting text into a list of strings 
+def conv(stx ) :
+    cur ="" 
+    lis = [] 
+    for i in stx :
+      
+        if (i ==" ") :
+            if cur != "" or cur != " " : 
+                lis.append(cur) 
+            
+            cur = "" 
+            continue 
+        cur = cur+ i 
+
+    if cur != "" and cur != " " : 
+        lis.append(cur) 
+            
+            
+    
+    return lis 
+
+
+# for converting the list of strings into text
+def rev_con(lis ) :
+    ans = ""
+    for i in lis  :
+        ans = ans+" " + i 
+    return  ans 
 
 
 
@@ -9,5 +65,84 @@ import subprocess
 def run() :
     subprocess.call([r'command.bat'])
 
-run() 
+
+
+
+# for finding and declaring keywords 
+
+def val( sr ) :
+    global count 
+    if sr in dic.keys() :
+        return dic[sr] 
+    count += 1 
+
+    dic[sr] = "as" + str(count) 
+
+    rev_dic[dic[sr]] = sr 
+
+    return dic[sr] 
+
+
+#for converting hindi text to english along with the added files 
+
+def hindi_eng( strin ) :
+    
+    lis = conv(strin ) 
+
+    #for i in lis : 
+    #    print( i )
+
+    out_lis =[] 
+    
+    
+
+    for i in lis : 
+        cnn =  val(i) 
+        print(cnn)
+        out_lis.append(cnn ) 
+    
+  
+
+    out =  rev_con(out_lis ) 
+    return out
+
+
+
+
+
+
+#for converting the english to hindi along wiht the added files 
+def eng_hindi(strin ) : 
+    lis = conv(strin ) 
+    out_lis =[] 
+
+    for i in lis : 
+        cnn =  rev_dic[i]
+        out_lis.append(cnn ) 
+
+    out =  rev_con(out_lis ) 
+    return out 
+
+def doi() :
+    f = open("flags\input.txt", "r" , encoding='utf-8')
+    st =  f.read() 
+
+
+
+
+    
+    
+    st = hindi_eng(st ) 
+    
+    print(st)
+    out = open("code.py" , "w")
+    out.write(st) 
+
+    out.close() 
+    f.close() 
+
+
+ 
+doi() 
+
 
--- hindi_english/to_map.py
+++ hindi_english/to_map.py
@@ -2,6 +2,7 @@ import json
 
 dic = {} 
 
+rev_dic = {} 
 
 
 
@@ -11,8 +12,11 @@ for i in range(0 ,cnt ) :
     y = input()
     z = input()
     dic[y] = z 
+    rev_dic[z] = y 
 
 
 
 with open ("map.json" ,"w") as out : 
     json.dump(dic , out )  
+with open ("rev_map.json" ,"w") as out : 
+    json.dump(rev_dic , out )  
